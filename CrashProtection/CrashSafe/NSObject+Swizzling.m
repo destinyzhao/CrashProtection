55147554a2d13a3135b55467dba4be4574a660b5
Destiny <346276712@qq.com>
2018-03-05 11:22:44 +0800

    update

--- CrashProtection/CrashSafe/NSObject+Swizzling.m
+++ CrashProtection/CrashSafe/NSObject+Swizzling.m
@@ -17,16 +17,21 @@
     
     Method originalMethod = class_getInstanceMethod(selfClass, originalSelector);
     Method swizzledMethod = class_getInstanceMethod(selfClass, swizzledSelector);
+    
+    // 动态添加方法 Class cls, SEL name, IMP imp, const char types
     BOOL didAddMethod = class_addMethod(selfClass,
                                         originalSelector,
                                         method_getImplementation(swizzledMethod),
                                         method_getTypeEncoding(swizzledMethod));
+    // 如果动态方法添加成功
     if (didAddMethod) {
+        // 如果方法存在替换方法
         class_replaceMethod(selfClass,
                             swizzledSelector,
                             method_getImplementation(originalMethod),
                             method_getTypeEncoding(originalMethod));
     } else {
+        // 交换两个方法实现
         method_exchangeImplementations(originalMethod, swizzledMethod);
     }
 }
